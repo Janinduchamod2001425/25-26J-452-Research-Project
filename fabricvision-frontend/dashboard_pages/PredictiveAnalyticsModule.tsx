06ad57fb13cb3f111b476b73991cc4e678b6c177
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-11-30 10:44:48 +0530

    Create PredictiveAnalyticsModule.tsx

---
+++ fabricvision-frontend/dashboard_pages/PredictiveAnalyticsModule.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PredictiveAnalyticsModule = () => {
+  return (
+    <div className="flex items-center h-full text-sm font-semibold text-gray-800">
+      PredictiveAnalyticsModule
+    </div>
+  );
+};
+export default PredictiveAnalyticsModule;
