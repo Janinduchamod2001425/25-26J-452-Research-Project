481ab948b8f0e2ea9895191e808c658dade4470d
ThisaraJayas <sasmithajayasinghe1@gmail.com>
2025-12-02 22:11:47 +0530

    fabric detection module

--- fabricvision-frontend/dashboard_pages/DefectDetectionModule.tsx
+++ fabricvision-frontend/dashboard_pages/DefectDetectionModule.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const DefectDetectionModule = () => {
   return (
     <div className="flex items-center h-full text-sm font-semibold text-gray-800">
-      DefectDetectionModule
+      DefectDetectionModulemmm
     </div>
   );
 };
