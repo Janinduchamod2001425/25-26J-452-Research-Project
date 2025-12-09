6aa74e5ae13b5e475a1f8a7cb33eed9f872ac60a
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-12-09 17:57:19 +0530

    add top bar in dashboard page

--- fabricvision-frontend/dashboard_pages/FogComputingModule.tsx
+++ fabricvision-frontend/dashboard_pages/FogComputingModule.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-
-const FogComputingModule = () => {
-  return (
-    <div className="flex items-center h-full text-sm font-semibold text-gray-800">
-      FogComputingModule
-    </div>
-  );
-};
-export default FogComputingModule;
+// Dashboard view
