97045c94a3a80d737ea22d43a43f8b28bdc05a7b
Duvidu kavinda <152588529+duvidu@users.noreply.github.com>
2026-03-09 06:35:36 +0530

    model B correction

--- fabapi/services/modelb/router.py
+++ fabapi/services/modelb/router.py
@@ -1,27 +1,16 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from .modelB_service import run_modelB
 
-from .modelB_service import predict_modelB
+router = APIRouter(prefix="/modelB", tags=["Model B"])
 
-router = APIRouter(prefix="/modelB", tags=["Model B – Risk & RCA"])
+class SupplierInput(BaseModel):
+    supplier:str
 
-class ModelBRequest(BaseModel):
-    SupplierEnc: int
-    RollLength: float
-    DefectCount: int
-    AvgSeverity: float
-    DefectDensity: float
-    MeanInterval: float
-    StdInterval: float
 
-@router.post("/predict")
-def modelB_predict(req: ModelBRequest):
-    result = predict_modelB(req.dict())
+@router.post("/analyze")
+def analyze_supplier(data:SupplierInput):
 
-    return {
-        "success": True,
-        "risk_score": result["risk_score"],
-        "pattern_class": result["pattern_class"],
-        "rca_class": result["rca_class"],
-        "message": "MODEL B prediction successful"
-    }
+    result = run_modelB(data.supplier)
+
+    return result
