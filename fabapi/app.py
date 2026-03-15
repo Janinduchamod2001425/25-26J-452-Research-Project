0b58662992ddf9f9b5e6a687494c293f3917b0bb
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-03-15 10:48:01 +0530

    updated

--- fabapi/app.py
+++ fabapi/app.py
@@ -383,10 +383,11 @@ async def general_exception_handler(request, exc):
     )
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     uvicorn.run(
         socket_app,
         host="0.0.0.0",
-        port=5000,
-        reload=True,
+        port=port,
         log_level="info"
     )
